7b405193ef20926ec1e438b917edc10301a65b41
Ashok Kumar <ashok.ayyarchamy@mvpfactory.co>
2025-07-29 18:38:09 +0200

    feat(): frontend moved to a separate folder

--- backend/app/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.endpoints import health
-
-api_router = APIRouter()
-
-# Include all endpoint routers
-api_router.include_router(health.router, tags=["health"]) 
